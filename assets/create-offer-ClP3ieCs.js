c25bd54616e25c01981f7130aef9eaca08808a0a
amalkurbanbaev <amalkurbanbaev@yandex.ru>
2025-06-05 15:59:11 +0500

    Updates

---
+++ assets/create-offer-ClP3ieCs.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-NM0xq0J4.js";function t(){return e.jsx("section",{className:"container",children:"Hello, Create Offer View!"})}const o=t;export{o as component};
---
+++ assets/index-BHwP7XUC.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-NM0xq0J4.js";function n(){return o.jsx("section",{className:"container",children:"Hello, NFT view!"})}const t=n;export{t as component};
