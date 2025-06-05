bd53132011da7c49893f40868d214b2c90e69738
amalkurbanbaev <amalkurbanbaev@yandex.ru>
2025-06-05 16:01:16 +0500

    feat: setting up gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/web3-course/",
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
