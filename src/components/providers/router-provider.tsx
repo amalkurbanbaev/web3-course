18db93c68a32d4eee186e4836558116182431979
amalkurbanbaev <amalkurbanbaev@yandex.ru>
2025-06-05 16:20:39 +0500

    feat: hash router

--- src/components/providers/router-provider.tsx
+++ src/components/providers/router-provider.tsx
@@ -2,16 +2,22 @@ import { routeTree } from "@/generated/routes.gen"
 import {
   ErrorComponent,
   RouterProvider as TanStackRouterProvider,
+  createMemoryHistory,
   createRouter,
 } from "@tanstack/react-router"
 import PageLoader from "../layouts/page-loader"
 import { queryClient } from "./query-provider"
+
+const memoryHistory = createMemoryHistory({
+  initialEntries: ["/"],
+})
 
 export const router = createRouter({
   routeTree,
   defaultPendingComponent: () => <PageLoader />,
   defaultErrorComponent: ({ error }) => <ErrorComponent error={error} />,
   defaultNotFoundComponent: () => <div>Not Found</div>,
+  history: memoryHistory,
   context: {
     queryClient,
   },
